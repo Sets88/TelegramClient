655c2a28929b91e8417d578000d260824e099209
Maxim <sets88@mail.ru>
2018-08-31 09:03:50 +0300

    Create client.sh

---
+++ client.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+export TG_API_ID=INSERT_ID_HERE
+export TG_API_HASH="INSERT_HASH_HERE"
+export TG_PHONE="INSERT_PHONE_NUMBER"
+export TG_USERNAME="INSERT_USERNAME"
+export TG_ACCESS_HASH=INSERT_ACCESS_HASH
+killall python3
+nohup python3 client1.py
